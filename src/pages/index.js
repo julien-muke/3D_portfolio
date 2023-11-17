6ac2cd1cb4b3084f6ef09bdb2239e23f58810550
julien-muke <julien.muke@gmail.com>
2023-11-17 05:57:25 -0800

    🚀 deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,11 @@
-import CTA from "./CTA";
-import Alert from "./Alert";
-import Loader from "./Loader";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-import HomeInfo from "./HomeInfo";
+import Home from "./Home";
+import About from './About'
+import Projects from "./Projects";
+import Contact from "./Contact";
 
 export {
-    CTA,
-    Alert,
-    Loader,
-    Navbar,
-    Footer,
-    HomeInfo
+    Home,
+    About,
+    Projects,
+    Contact
 }
